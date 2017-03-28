fbd56e57a12d39414adef54f6b650b39f2259198
MD-Lai <marvin.dhlai@gmail.com>
2017-03-29 03:34:32 +1100

    finish report

--- funcs.py
+++ funcs.py
@@ -1,10 +1,12 @@
 '''
+By Marvin Lai - 754672
 Functions written for Comp30027 - Machine Learning Project 1
 Specifically written to process the abalone dataset
 Requires random library to work 
 Written for Python 3.4
 '''
 import random
+
 #takes raw data and returns it as a tuple consisting of 
 #([attributes], [[data1],[data2],...,[datan]])
 def preprocess_data(dir):
@@ -31,13 +33,16 @@ def preprocess_data(dir):
 			thisLine[i] = float(thisLine[i])
 
 		#bin at start or bin at end...that is the question
-		#i say bin at start
+		#i say bin at end
 		thisLine[len(thisLine) - 1] = bin(float(thisLine[len(thisLine) - 1]), 1)
 		#thisLine[len(thisLine) - 1] = float(thisLine[len(thisLine) - 1])
 		data[1].append(thisLine)
 	f.close()
 	return data
 
+#places abalone in the appropriate categories or "bins" 
+#if set to 1, does nothing, otherwise, 
+#can do abalone-2 or abalone-3 accordingly
 def bin(rings, abMode = 2):
 	if(abMode == 1):
 		return rings
@@ -54,6 +59,7 @@ def bin(rings, abMode = 2):
 			return 2
 
 	return -1
+
 #compares a test instance to a training instance
 #using method (default euclidean dist) to calculate similarity (lower is closer)
 #with optional attibutes to exclude in comparisons specified as array of strings
@@ -163,11 +169,12 @@ def predictClass(neighbours, method = 'knn'):
 	if(method == "knn"):
 		
 		tracker = {}
-		for clsScore in neighbours:
-			if(clsScore[0] in tracker):
-				tracker[clsScore[0]] += 1#.append(clsScore)
+
+		for neighbour in neighbours:
+			if(neighbour[0] in tracker):
+				tracker[neighbour[0]] += 1#.append(neighbour)
 			else:
-				tracker[clsScore[0]] = 1#[clsScore]
+				tracker[neighbour[0]] = 1#[neighbour]
 
 		items = []
 		for trackerKeys in tracker.keys():
@@ -188,7 +195,7 @@ def predictClass(neighbours, method = 'knn'):
 
 		#once sorted, return item at front, 
 		#which is the class with most neighbours OR
-		#in case of tie, due to use of dict, result is a little random...
+		#in case of tie, return first item in neighbours
 		return items[0][0]
 
 
@@ -234,7 +241,7 @@ def evaluate(data_set, metric = "accuracy", p = 19, nn = 7):
 				
 				(bin(
 					predictClass(
-						getNeighbours(
+						getNeighbours( #change "euc" or "mnh" or "cos" here
 							testInstance,trainSet, nn, "euc", ["Rings"]), "knn")
 					, 2), 
 				bin(testInstance[attributes.index("Rings")] ,2))
@@ -286,7 +293,7 @@ def evaluate(data_set, metric = "accuracy", p = 19, nn = 7):
 	if(metric == "all"):
 		return [accuracy, error_rate, precision, sensitivity, specificity]
 
-
+#helper function to show that it's alive between each iteration of k
 def printPredictions(predicts):
 	nYoung = 0
 	nOld = 0
@@ -309,10 +316,9 @@ def printPredictions(predicts):
 	print("n Yng", nYoung, "c Yng", cYoung)
 	print("n Old", nOld, "c Old", cOld)
 
-def doAll(mode = "print"):
-
-	folds = [19]
-	nei = [7]
+#use this function with mode = "file" to print to file,
+#otherwise prints to console if anything but mode = "file" is given
+def doAll(mode = "print", folds = [19], nei = [7]):
 	acc = []
 
 	for p in folds:
@@ -348,4 +354,4 @@ def doAll(mode = "print"):
 	f.close()
 
 #use this line to view the full output of the program
-doAll("file")
+#doAll("file")
